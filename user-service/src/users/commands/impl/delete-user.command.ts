0950ab4f9382260858380ba49fcebbf93d4e2187
Felipe Appio <felipexw@gmail.com>
2021-03-28 20:52:57 -0300

    Remove unused code

--- user-service/src/users/commands/impl/delete-user.command.ts
+++
@@ -1,8 +0,0 @@
-import { ICommand } from '@nestjs/cqrs';
-import { UserIdRequestParamsDto } from '../../dtos/users.dto';
-
-export class DeleteUserCommand implements ICommand {
-  constructor(
-    public readonly userDto: UserIdRequestParamsDto,
-  ) {}
-}
